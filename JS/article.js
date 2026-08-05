537691e79aee908eaa7bf5f27c968f2617073b88
tt-huang <summertaitai@gmail.com>
2026-08-05 10:23:32 +0900

    Create dynamic article cards with JS

---
+++ JS/article.js
@@ -0,0 +1,21 @@
+//建立 articles 陣列
+const articles = [
+  {
+    id: 1,
+    category: "cafe",
+    title: "三間值得再訪的咖啡店",
+    date: "2026.08.03",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJutnadDh4TSJfwh3sPjJRTDnS3BBncpLF8NhDkUKFyw&s=10",
+    tags: ["Kuramae", "Dessert"],
+  },
+
+  {
+    id: 2,
+    category: "walk",
+    title: "上野散步半日遊",
+    date: "2026.08.03",
+    image: "https://images.unsplash.com/photo-1611065695756-1d50152352e5?w=900",
+    tags: ["Ueno", "Park"],
+  },
+];
--- JS/explore.js
+++ JS/explore.js
@@ -1,40 +1,74 @@
-console.log("JS開始執行");
+//＊＊＊＊＊ 讀取資料內容 ＊＊＊＊＊
 
-// ===============================
-// 取得 Filter 按鈕
-// ===============================
+// 找到 HTML 裡的 container class
+const container = document.querySelector(".container");
+// console.log(container);
 
-// 找到所有分類按鈕
+//把 articles 裡面的每一筆資料拿出來，暫時叫它 article
+articles.forEach((article) => {
+  // console.log(article);
+
+  //請 JS 幫我建立一個 <a> 標籤
+  const card = document.createElement("a");
+  //加上 card-link class
+  card.classList.add("card-link");
+  // console.log(card);
+
+  // 設定點擊後要前往的網址
+  card.href = `article.html?id=${article.id}`;
+
+  //把 HTML 放到 <a> 裡面
+  card.innerHTML = `
+  <article class="card">
+
+    <div class="card-photo">
+      <img src="${article.image}">
+    </div>
+
+    <p class="category">${article.category}</p>
+
+    <h1>${article.title}</h1>
+
+    <p class="date">${article.date}</p>
+
+   
+  <div class="tags">
+      ${article.tags.map((tag) => `<span>${tag}</span>`).join("")}
+    </div>
+
+
+  </article>
+`;
+  //把剛剛建立好的 card 放進 <section class="container">
+  container.appendChild(card);
+});
+
+//＊＊＊＊＊ filter 程式 ＊＊＊＊＊
+
+// 找到所有分類按鈕: <button class="filter-btn">
 const filterButtons = document.querySelectorAll(".filter-btn");
-
-// ===============================
-// 取得文章卡片
-// ===============================
-
-// 找到所有文章卡片
+// 找到所有文章卡片: <div class="card">
 const cards = document.querySelectorAll(".card");
-
-// ===============================
-// Filter 功能
-// ===============================
-
-// 讓每一個按鈕都監聽 click
+// forEach(): 對每一個分類按鈕都加入點擊事件
 filterButtons.forEach((button) => {
+  // 當按鈕被點擊時
   button.addEventListener("click", () => {
-    // 取得目前點擊按鈕的分類
-    const category = button.dataset.category;
+    // 取得目前按鈕的分類 (all、cafe、spot...)
+    //button.dataset : 取得這顆按鈕所有 data-* 的資料
 
-    console.log("選擇分類:", category);
+    const category = button.dataset.category;
 
     // 檢查每一張卡片
     cards.forEach((card) => {
-      // 取得卡片自己的分類
+      // 取得目前這張卡片的分類
+      // 例如:cardCategory =  Cafe , Walk .....(新㽪卡片)
       const cardCategory = card.dataset.category;
 
-      // 如果選 All
-      // 或是按鈕分類跟卡片分類相同
-      if (category === "all" || category === cardCategory) {
+      // 如果按的是all ， 或按鈕和卡片相同
+      if (category == "all" || category === cardCategory) {
         // 顯示卡片
+        //元素.style.屬性 = "值";
+        // 修改卡片的 CSS display 屬性，讓卡片顯示
         card.style.display = "block";
       } else {
         // 隱藏卡片
